58500c9344a4f5be36793db3ae65041cae0a0b38
Ali-AlDhamen <ali77dhamen@hotmail.com>
2024-03-12 19:57:43 +0300

    flashcards learning screen

---
+++ lib/features/flashcards/view/flashcard_learning_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FlashCardLearning extends ConsumerWidget {
+  const FlashCardLearning({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.5),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: RichText(
+            text: const TextSpan(
+              text: '1',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                  text: '/10',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(5),
+          child: LinearProgressIndicator(
+            value: 0.5,
+            backgroundColor: Colors.grey.withOpacity(0.5),
+            valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
+          ),
+        )
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 1,
+                      blurRadius: 2,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: const [
+                    SizedBox(height: 8),
+                    Text(
+                      'Twjwebjkewj jwdbiwbe dwbih bew bdwihbwie erm',
+                      textAlign: TextAlign.center,
+                    ),
+                    Divider(),
+                    Text(
+                      'lorem ewbbweibwe dwejhhdw xhwbiwbwx xbwihb edhbdwi dhwbhew ehbbdhbwd',
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                  
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      primary: Colors.green,
+                    ),
+                    onPressed: () {},
+                    child: const Text('Mastered', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                       shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      primary: Colors.red,
+                    ),
+                    onPressed: () {},
+                    child: const Text('Need Review', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );  
+  }
+}
