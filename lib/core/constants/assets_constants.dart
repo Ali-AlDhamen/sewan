048d07a7cba1f646ba06a3393d27987ad504e1f5
Ali-AlDhamen <ali77dhamen@hotmail.com>
2024-03-12 19:55:42 +0300

    AssetsConstants

---
+++ lib/core/constants/assets_constants.dart
@@ -0,0 +1,6 @@
+class AssetsConstants {
+  static String toastFailIcon = "assets/images/toast_fail.png";
+  static String toastSuccessIcon = "assets/images/toast_win.png";
+  static String flashcardsIcon = "assets/images/flashcards.png";
+
+}
