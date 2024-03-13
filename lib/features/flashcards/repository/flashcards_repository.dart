db97b62a793c33d25a241fc8179a450d541e9d89
Ali-AlDhamen <ali77dhamen@hotmail.com>
2024-03-13 20:57:53 +0300

    flashcard repository

--- lib/features/flashcards/repository/flashcards_repository.dart
+++ lib/features/flashcards/repository/flashcards_repository.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:sewan/core/constants/firebase_constants.dart';
+import 'package:sewan/core/models/course_model.dart';
 import 'package:sewan/core/models/flashcard_model.dart';
 import 'package:sewan/core/models/lecture_model.dart';
 import 'package:sewan/core/providers/firebase_providers.dart';
@@ -21,22 +22,69 @@ class FlashCardsRepository {
     required FirebaseFirestore firestore,
   }) : _firestore = firestore;
 
-  CollectionReference get _flashCards =>
+  CollectionReference get _lectures =>
       _firestore.collection(FirebaseConstants.lecturesCollection);
+  
+  CollectionReference get _courses =>
+      _firestore.collection(FirebaseConstants.coursesCollection);
+
+  CollectionReference get _users =>
+      _firestore.collection(FirebaseConstants.usersCollection);
+  
+  
+
+
 
   FutureVoid uploadLecture(LectureModel lecture) async {
     try {
-      return right(await _flashCards.doc(lecture.id).set(lecture.toMap()));
+      return right(
+        _users.doc(lecture.userId).collection(FirebaseConstants.lecturesCollection).doc(lecture.id).set(lecture.toMap())
+      );
     } on FirebaseException catch (e) {
-      return Left(Failure(e.toString()));
+      return left(Failure(e.toString()));
     } catch (e) {
       return left(Failure(e.toString()));
     }
   }
 
+  FutureVoid createCourse(CourseModel course) async{
+    try {
+      return right(
+        _users.doc(course.userId).collection(FirebaseConstants.coursesCollection).doc(course.id).set(course.toMap())
+      );
+    } on FirebaseException catch (e) {
+      return left(Failure(e.toString()));
+    } catch (e) {
+      return left(Failure(e.toString()));
+      
+    }
+  }
+
+  Future<List<CourseModel>> getCourses(String userId) async {
+    try {
+      final snapshot = await _users.doc(userId).collection(FirebaseConstants.coursesCollection).get();
+      return snapshot.docs.map((e) => CourseModel.fromMap(e.data())).toList();
+    } on FirebaseException catch (e) {
+      throw Failure(e.toString());
+    } catch (e) {
+      throw Failure(e.toString());
+    }
+  }
+
+  Future<List<LectureModel>> getCourseLectures(String userId, String courseId) async {
+    try {
+      final snapshot = await _users.doc(userId).collection(FirebaseConstants.coursesCollection).doc(courseId).collection(FirebaseConstants.lecturesCollection).get();
+      return snapshot.docs.map((e) => LectureModel.fromMap(e.data())).toList();
+    } on FirebaseException catch (e) {
+      throw Failure(e.toString());
+    } catch (e) {
+      throw Failure(e.toString());
+    }
+  }
+
   FutureVoid addFlashCard({required String lectureId ,required List<FlashCardModel> flashCards}) async {
     try {
-      return right(await _flashCards.doc(lectureId).update({
+      return right(await _lectures.doc(lectureId).update({
         'flashCards': FieldValue.arrayUnion(flashCards.map((e) => e.toMap()).toList())
       }));
     } on FirebaseException catch (e) {
