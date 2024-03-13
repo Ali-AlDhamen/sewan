82e22699215d08a0b70d7259e9876e528dbc3e71
Ali-AlDhamen <ali77dhamen@hotmail.com>
2024-03-13 20:58:27 +0300

    lectures screen

--- lib/features/flashcards/view/lectures_screen.dart
+++ lib/features/flashcards/view/lectures_screen.dart
@@ -1,22 +1,28 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sewan/features/flashcards/controller/flashcards_controller.dart';
 import 'package:sewan/features/flashcards/view/widgets/lecture_widget.dart';
 
 class LecturesScreen extends ConsumerWidget {
-  const LecturesScreen({super.key});
+  final String courseId;
+  const LecturesScreen({super.key, required this.courseId});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Lectures'),
+        title: Text('Course Name'),
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return const LectureWidget();
+      body: ref.watch(courseLecturesProvider(courseId)).when(
+        data: (data) {
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return LectureWidget(lecture: data[index]);
+            },
+          );
         },
+        error: (e, st) => Center(child: Text(e.toString())),
+        loading: () => const Center(child: CircularProgressIndicator()),
       ),
     );
   }
